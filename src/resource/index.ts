48bf318baae388ab24b878a7eefa5e4a6f9acd4e
Caian R. Ertl <hi@caian.org>
2020-05-02 21:59:37 -0300

    feat: auxiliary function to send response message

--- src/resource/index.ts
+++ src/resource/index.ts
@@ -1,5 +1,21 @@
+import { APIGatewayProxyResult as Result } from 'aws-lambda';
+
 export {
     APIGatewayProxyEvent as Event,
     APIGatewayEventRequestContext as Context,
     APIGatewayProxyResult as Result,
 } from 'aws-lambda';
+
+interface IResult
+{
+    [key: string]: (body: string) => Result;
+}
+
+export function sendResponse(): IResult
+{
+    const result = (body: string, statusCode: number): Result => { return { body, statusCode }; };
+
+    const ok = (body: string): Result => { return result(body, 200); };
+
+    return { ok };
+}
