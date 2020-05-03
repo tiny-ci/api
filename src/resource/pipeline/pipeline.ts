97bb44984958618124fb08afc983d084a9111b6c
Caian R. Ertl <hi@caian.org>
2020-05-02 22:14:00 -0300

    feat: use sendResponse object

--- src/resource/pipeline/pipeline.ts
+++ src/resource/pipeline/pipeline.ts
@@ -1,15 +1,17 @@
-import { Event, Context, Result } from '../';
+import { Event, Context, Result, sendResponse } from '../';
 import { IResourceHandler } from '../../types';
 import { MongoDb } from '../../db';
 
 export function PipelineHandler(db: MongoDb): IResourceHandler
 {
+    const send = sendResponse();
+
     const get = async (event: Event, context: Context): Promise<Result> => {
         console.log(event);
         console.log(context);
         console.log(db.isConnected());
 
-        return { statusCode: 200, body: '' };
+        return send.ok('');
     };
 
     return { get };
