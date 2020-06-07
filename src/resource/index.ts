957d40b3f3aab4e43dc10195e6d1a683959efd16
Caian R. Ertl <hi@caian.org>
2020-06-07 06:38:03 -0300

    feat: malformed (400) and success (200) responses

--- src/resource/index.ts
+++ src/resource/index.ts
@@ -8,7 +8,7 @@ export {
 
 interface IResult
 {
-    [key: string]: (body: string) => Result;
+    [key: string]: (body?: string) => Result;
 }
 
 export function sendResponse(): IResult
@@ -16,6 +16,18 @@ export function sendResponse(): IResult
     const result = (body: string, statusCode: number): Result => { return { body, statusCode }; };
 
     const ok = (body: string): Result => { return result(body, 200); };
+    const badRequest = (body: string): Result => { return result(body, 400); };
 
-    return { ok };
+    const malformed = (): Result => {
+        return badRequest(JSON.stringify({
+            message: 'received payload is malformed'
+        }));
+    };
+
+    const success = (body?: string): Result => {
+        if (!body) body = '';
+        return ok(body);
+    };
+
+    return { success, malformed };
 }
