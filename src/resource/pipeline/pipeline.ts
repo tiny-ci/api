ad94e68bd93c90ed9750c49c873ba02d955e7e4a
Caian R. Ertl <hi@caian.org>
2020-06-20 03:39:41 -0300

    feat(pipeline): check event body against schema

--- src/resource/pipeline/pipeline.ts
+++ src/resource/pipeline/pipeline.ts
@@ -1,14 +1,33 @@
+import { log } from '../../';
+
 import { Event, Context, Result, IResourceHandler } from '../../lib/types';
-import { MongoDb } from '../../db';
-import { send } from '../response';
+import { MongoDb } from '../../db/MongoDb';
+import { PipelineModel } from '../../db/models/pipeline';
+import { ExErr, send } from '../response';
 
 import { PipelineCreateSchemaValidator } from './schema';
 
 export function PipelineHandler(db: MongoDb): IResourceHandler
 {
-    const get = async (event: Event, context: Context): Promise<Result> => {
+    const model = PipelineModel(db);
+
+    async function get(event: Event, context: Context): Promise<Result>
+    {
+        log.info(`body (${typeof(event.body)}): ${event.body}`);
+        const pipelinePayload = new PipelineCreateSchemaValidator(event.body);
+
+        if (!pipelinePayload.isValid()) {
+            log.error(ExErr.InvalidEventBody);
+
+            pipelinePayload.getErrors().forEach((error: string): void => {
+                log.error(`- ${error}`);
+            });
+
+            return send.malformed();
+        }
+
         return send.success();
-    };
+    }
 
     const post = async (event: Event, context: Context): Promise<Result> => {
         const body = event.body;
